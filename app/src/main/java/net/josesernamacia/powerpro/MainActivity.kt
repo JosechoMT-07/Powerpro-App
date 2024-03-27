c703c1923ea4e3a9e6aa76088d5f4b314b496c58
JosechoMT-07 <jossermac@alu.edu.gva.es>
2024-03-27 20:26:13 +0100

    Creación del proyecto

---
+++ app/src/main/java/net/josesernamacia/powerpro/MainActivity.kt
@@ -0,0 +1,11 @@
+package net.josesernamacia.powerpro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
