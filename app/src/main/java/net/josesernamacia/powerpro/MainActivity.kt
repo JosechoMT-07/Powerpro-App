44dd2ad031a54418eb2003da1916075a226ce09f
JosechoMT-07 <jossermac@alu.edu.gva.es>
2024-03-31 16:45:40 +0200

    Creación de menú de navegación con los diferentes fragments y activitys

--- app/src/main/java/net/josesernamacia/powerpro/MainActivity.kt
+++ app/src/main/java/net/josesernamacia/powerpro/MainActivity.kt
@@ -2,10 +2,41 @@ package net.josesernamacia.powerpro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var  bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId){
+                R.id.bottom_you -> {
+                    replaceFragment(YouFragment())
+                    true
+                }
+                R.id.bottom_news -> {
+                    replaceFragment(NewsFragment())
+                    true
+                }
+                R.id.bottom_modules -> {
+                    replaceFragment(ModulesFragment())
+                    true
+                }
+
+                else -> false
+            }
+        }
+        replaceFragment(YouFragment())
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit()
     }
 }
---
+++ app/src/main/java/net/josesernamacia/powerpro/ModulesFragment.kt
@@ -0,0 +1,20 @@
+package net.josesernamacia.powerpro
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class ModulesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_modules, container, false)
+    }
+
+}
