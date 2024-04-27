a056f0ce1dc51d7f039608ddb5345b2b4dce2a1a
JosechoMT-07 <jossermac@alu.edu.gva.es>
2024-04-27 14:55:19 +0200

    Con acceso al botón de sola

--- app/src/main/java/net/josesernamacia/powerpro/ModulesFragment.kt
+++ app/src/main/java/net/josesernamacia/powerpro/ModulesFragment.kt
@@ -1,23 +1,32 @@
 package net.josesernamacia.powerpro
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 
 
 class ModulesFragment : Fragment() {
+
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_modules, container, false)
-
+        val v: View = inflater.inflate(R.layout.fragment_modules, container, false)
+        val btn: Button = v.findViewById(R.id.btSolar)
+        btn.setOnClickListener {
+            val intent = Intent(context, PhotovoltaicActivity::class.java)
+            startActivity(intent)
+        }
+        return v
     }
-
-
 
 }
